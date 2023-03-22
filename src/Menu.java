1dddb0b2779fcd5052ba9f772c62af8c7e780b06
SebastianZulj <127675391+SebastianZulj@users.noreply.github.com>
2023-03-22 14:29:40 +0100

    Lagt till funktion som visar meny i terminalen

---
+++ src/Menu.java
@@ -0,0 +1,10 @@
+public class Menu {
+
+    public void showMenu() {
+        System.out.println("1. Välj bok.");
+        System.out.println("2. Sälj bok.");
+        System.out.println("3. Anna Book.");
+        System.out.println("0. Avsluta program");
+    }
+
+}
