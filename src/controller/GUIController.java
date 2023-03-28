3837ed4f32597dd75fc051399e7ab701ea7b7cb7
SebastianZulj <127675391+SebastianZulj@users.noreply.github.com>
2023-03-28 12:15:17 +0200

    Create GUIController.java

---
+++ src/controller/GUIController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import View.Menu;
+
+/**
+ * This class communicates with the User Interface and interprets the menu choices the user makes.
+ * @author Sebastian Zulj
+ */
+
+public class GUIController {
+
+    public Menu menu;
+
+    public GUIController(Menu menu) {
+        this.menu = menu;
+    }
+
+    /**
+     * This method interprets
+     */
+    public void makeChoice() {
+
+    }
+
+
+}
