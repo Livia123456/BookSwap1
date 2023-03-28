22429ccb145e54b9c2651206a843024e529b8d5a
kappppe <kasperlindberg91@gmail.com>
2023-03-28 12:17:47 +0200

    Update BookHandler.java

--- src/controller/BookHandler.java
+++ src/controller/BookHandler.java
@@ -3,14 +3,37 @@ package controller;
 import model.Book;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class BookHandler {
 
     private ArrayList<Book> books;
 
+    private ArrayList<Book> searchBooks;
+
     public BookHandler(){
         books = new ArrayList<Book>();
     }
 
+    public ArrayList<Book> searchBooks(String search) {
+
+        for (int i = 0; i < books.size(); i++) {
+
+            if (books.get(i).getTitle().equals(search)) {
+                searchBooks.add(books.get(i));
+            }
+            else if (books.get(i).getReleaseDate().equals(search)) {        // "parsa"
+                searchBooks.add(books.get(i));
+            }
+            else if (books.get(i).getISBN().equals(search)){
+                searchBooks.add(books.get(i));
+            }
+            else if (books.get(i).getGenre().equals(search)){       // "parsa"
+                searchBooks.add(books.get(i));
+            }
+            
+        }
+        return searchBooks;
+    }
 
 }
